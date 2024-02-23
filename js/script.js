21f917480af85a76cdb0e02690dedb003414888a
Alessandro-Moroni <alemoro98@gmail.com>
2024-02-23 17:11:41 +0100

    [finish]all

--- js/script.js
+++ js/script.js
@@ -17,13 +17,31 @@ if(verifica){
 <p>
   La tua email <strong> ${userEmail} </strong> è corretta.
 </p>
-`
+`;
 }else{
   alert('Email non corretta, riprovare');
+  const bt1 = document.querySelector('.bt1');
+  bt1.classList.add('d-none');
 }
 
+const bt1 = document.querySelector('.bt1');
 
-
+bt1.addEventListener('click',function(){
+  
+  const dadoUser = (Math.ceil(Math.random() * 6));
+  const dadoPC = (Math.ceil(Math.random() * 6));
+  let result;
+  
+  if(dadoUser > dadoPC){
+    result = `Hai vinto te! con ${dadoUser} a ${dadoPC}`;
+  }else if(dadoUser < dadoPC){
+    result = `Ha vinto il Computer te! con ${dadoUser} a ${dadoPC}`;
+  }else{
+    result = `Il risultato è pari! con ${dadoUser} a ${dadoPC}`;
+  }
+  
+  document.getElementById('result').innerHTML = result;
+});
 
 
 
